b6b47c1b4ad083496158e08e8630122d41841b9f
MonishaKulal <monisha.vkb@gmail.com>
2023-08-14 10:28:25 +0530

    Added the new changes

--- Assignment_2/src/main/java/com/niveus/controller/HomeController.java
+++ Assignment_2/src/main/java/com/niveus/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "Welcome to the Springboot!";
     }
+        @GetMapping("/home")
+        public String base() {
+            return "Welcome to the Niveus!";
+        
+    }
 }
